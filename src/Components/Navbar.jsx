f56f01d54a2607e62199d74318bad52749a1b1d3
Austin Howard <cnfishead@hotmail.com>
2021-02-14 00:50:41 -0500

    Updates

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Home from "./Icons/home.png"
+import About from "./Icons/profile.png"
+import Skills from "./Icons/settings.png"
+import Icon from "./Icons/WulfBrandingLogoLightSmall.png"
+
+function Nav(){
+    return (
+    <div>
+      <header>
+        <div className = "navBar">
+        <div id="pageIcon">
+          <img src={Icon} alt="Page Icon"/>
+          
+        </div>
+        <h1>Wulf Developments</h1>
+        <div className = "pages">
+            <a href="#"><img src={Skills} alt="Skills"/></a>
+          </div>
+          <div className = "pages">
+            <a href="#"><img src={About} alt="About Me"/></a>
+          </div>
+          <div className = "pages">
+            <a href="#"><img src= {Home} alt="Home"/></a>
+          </div>
+        </div>
+        </header>
+    </div>)
+};
+
+export default Nav;
